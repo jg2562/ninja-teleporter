4888f000ec02cef81aedb331489c7ba65edcdba3
BRD-E <BRD-E@outlook.com>
2016-11-03 02:50:35 -0700

    Added more class
    Warrior and baller, so the way i plan on doing damage is weird but ill
    explain why in person.

--- __init__.py
+++ __init__.py
@@ -14,6 +14,14 @@ class Warrior(Person):
             #[0, 0, 0]           
             self._attack_area = [[2,3,2],[1, 0, 1], [0, 0, 0]]
             
+class Baller(Person):
+        def __init__(self, name, health, cooldown, pos, direction):
+            super(Warrior, self).__init__(name, health, cooldown, pos, direction)
+
+            #[2, 3, 2] 3 damage to guy directly infront of the player, 2 to guys on either side
+            #[1, 0, 1] 1 damage to guy left / right of the player
+            #[0, 0, 0]           
+            self._attack_area = [[0,15,0],[0, 0, 0], [0, 0, 0]] 
 x = Warrior("jef", 100, 4, [0,0], 0)
 
 print(x._attack_area)
